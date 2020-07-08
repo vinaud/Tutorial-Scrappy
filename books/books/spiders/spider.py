64ee6f6c21f7dd8f96cf182fba2e403b7a63ef6e
João Vinaud <leyenasd@gmail.com>
2020-07-08 10:12:10 -0300

    Armazenamento das informações coletadas em um JSON

--- books/books/spiders/spider.py
+++ books/books/spiders/spider.py
@@ -12,5 +12,11 @@ class SpiderSpider(scrapy.Spider):
         for book in all_books:
             title = book.xpath('.//h3/a/@title').extract_first()
             price = book.xpath('.//div/p[@class="price_color"]/text()').extract_first()
-            print(title)
-            print(price)
+            image_url = self.start_urls[0] + book.xpath('.//img[@class="thumbnail"]/@src').extract_first()
+            book_url = self.start_urls[0] + book.xpath('.//h3/a/@href').extract_first()
+            yield {
+                'title': title,
+                'price': price,
+                'Image URL': image_url,
+                'Book URL': book_url,
+            }
