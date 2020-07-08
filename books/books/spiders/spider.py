3de523069b2590e80ac9a3c38974a72c7d3d25b2
João Vinaud <leyenasd@gmail.com>
2020-07-08 10:09:27 -0300

    Extração de informações com xpath

--- books/books/spiders/spider.py
+++ books/books/spiders/spider.py
@@ -7,4 +7,10 @@ class SpiderSpider(scrapy.Spider):
     start_urls = ['http://books.toscrape.com/']
 
     def parse(self, response):
-        pass
+        all_books = response.xpath('//article[@class="product_pod"]')
+        
+        for book in all_books:
+            title = book.xpath('.//h3/a/@title').extract_first()
+            price = book.xpath('.//div/p[@class="price_color"]/text()').extract_first()
+            print(title)
+            print(price)
